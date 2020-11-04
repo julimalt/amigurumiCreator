7ab81fb5fa87f6c82cd1f8412f683af1625e9f95
julimalt <julietamaalt@gmail.com>
2020-11-04 01:15:11 -0300

    cambios js

--- amigurumi maker/functions.js
+++ amigurumi maker/functions.js
@@ -2,34 +2,36 @@ var precios1 = [0, 10, 15, 20];
 var precios2 = [0, 25, 50, 100];
 var precios3 = [0, 100, 150];
 
-var datos = {
-  nombre: undefined,
-  precio: 500,
-  precio1: 0,
-  precio2: 0,
-  precio3: 0,
-};
-
-function cambiarPrecio(valorElegido, array, atributo) {
-  var precioAgregado = array[valorElegido];
-  datos[atributo] = precioAgregado;
-  document.getElementById("precio").innerHTML =
-    datos.precio + datos.precio1 + datos.precio2 + datos.precio3;
+function Datos(nombre, precio, precio1, precio2, precio3) {
+  // Atributos
+  this.nombre = nombre;
+  this.precio = precio;
+  this.precio1 = precio1;
+  this.precio2 = precio2;
+  this.precio3 = precio3;
+  // Métodos
+  this.cambiarPrecio = function (valorElegido, array, atributo) {
+    var precioAgregado = array[valorElegido];
+    this[atributo] = precioAgregado;
+    document.getElementById("precio").innerHTML =
+      this.precio + this.precio1 + this.precio2 + this.precio3;
+  };
+  this.getOrden = function () {
+    var nombre = prompt("Ingresá tu nombre");
+    alert(
+      "Gracias " +
+        nombre +
+        " por tu compra!" +
+        " podes ver como quedó tu amigurumi"
+    );
+    this.nombre = nombre;
+    document.getElementById("nombre").innerHTML = this.nombre;
+    document.getElementById("costoColorPrincipal").innerHTML = this.precio1;
+    document.getElementById("costoColorSecundario").innerHTML = this.precio2;
+    document.getElementById("costoMedida").innerHTML = this.precio3;
+    document.getElementById("precioTotal").innerHTML =
+      this.precio + this.precio1 + this.precio2 + this.precio3;
+  };
 }
 
-function getOrden() {
-  var nombre = prompt("Ingresá tu nombre");
-  alert(
-    "Gracias " +
-      nombre +
-      " por tu compra!" +
-      " podes ver como quedó tu amigurumi"
-  );
-  datos.nombre = nombre;
-  document.getElementById("nombre").innerHTML = datos.nombre;
-  document.getElementById("costoColorPrincipal").innerHTML = datos.precio1;
-  document.getElementById("costoColorSecundario").innerHTML = datos.precio2;
-  document.getElementById("costoMedida").innerHTML = datos.precio3;
-  document.getElementById("precioTotal").innerHTML =
-    datos.precio + datos.precio1 + datos.precio2 + datos.precio3;
-}
+var nuevosDatos = new Datos(null, 500, 0, 0, 0);
